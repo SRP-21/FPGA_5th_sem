822e018264dd042f37cc1adb999aa1011f1390d7
Anwitha Vishwanath <162984701+anwitha-09@users.noreply.github.com>
2026-01-18 16:49:10 +0530

    Addition of NR module

--- Fusion_unit.v
+++ Fusion_unit.v
@@ -1,53 +1,44 @@
 module Fusion_unit (
     input  signed [15:0] P1_0,  P1_6,  P1_14, P1_21, P1_28, P1_36,
     input  signed [15:0] P2_0,  P2_6,  P2_14, P2_21, P2_28, P2_36,
-
     input  signed [15:0] X1_0, X1_1, X1_2, X1_3, X1_4, X1_5,
     input  signed [15:0] X2_0, X2_1, X2_2, X2_3, X2_4, X2_5,
-
     output signed [15:0] X0f, X1f, X2f, X3f, X4f, X5f,
     output signed [31:0] Pf1, Pf2, Pf3, Pf4, Pf5, Pf6
 );
 
-    wire signed [15:0] invt1, invt2, invt3, invt4, invt5, invt6;
+    /* Reciprocal of (P1 + P2) in Q15 format */
+    wire signed [15:0] inv0, inv1, inv2, inv3, inv4, inv5;
 
-    wire signed [31:0] w11, w21;
-    wire signed [31:0] w12, w22;
-    wire signed [31:0] w13, w23;
-    wire signed [31:0] w14, w24;
-    wire signed [31:0] w15, w25;
-    wire signed [31:0] w16, w26;
+    NR_div_w d0 (P1_0  + P2_0 , inv0);
+    NR_div_w d1 (P1_6  + P2_6 , inv1);
+    NR_div_w d2 (P1_14 + P2_14, inv2);
+    NR_div_w d3 (P1_21 + P2_21, inv3);
+    NR_div_w d4 (P1_28 + P2_28, inv4);
+    NR_div_w d5 (P1_36 + P2_36, inv5);
 
-    assign w11 = P2_0/(P1_0+P2_0);
-    assign w21 = P1_0/(P1_0+P2_0);
+    /* Numerator terms */
+    wire signed [31:0] num0 = P2_0  * X1_0 + P1_0  * X2_0;
+    wire signed [31:0] num1 = P2_6  * X1_1 + P1_6  * X2_1;
+    wire signed [31:0] num2 = P2_14 * X1_2 + P1_14 * X2_2;
+    wire signed [31:0] num3 = P2_21 * X1_3 + P1_21 * X2_3;
+    wire signed [31:0] num4 = P2_28 * X1_4 + P1_28 * X2_4;
+    wire signed [31:0] num5 = P2_36 * X1_5 + P1_36 * X2_5;
 
-    assign w12 = P2_6/(P1_6+P2_6);
-    assign w22 = P1_6/(P1_6+P2_6);
+    /* Fused state estimates */
+    assign X0f = (num0 * inv0) >>> 15;
+    assign X1f = (num1 * inv1) >>> 15;
+    assign X2f = (num2 * inv2) >>> 15;
+    assign X3f = (num3 * inv3) >>> 15;
+    assign X4f = (num4 * inv4) >>> 15;
+    assign X5f = (num5 * inv5) >>> 15;
 
-    assign w13 = P2_14/(P1_14+P2_14);
-    assign w23 = P1_14/(P1_14+P2_14);
-
-    assign w14 = P2_21/(P1_21+P2_21);
-    assign w24 = P1_21/(P1_21+P2_21);
-
-    assign w15 = P2_28 /(P1_28+P2_28);
-    assign w25 = P1_28/(P1_28+P2_28);
-
-    assign w16 = P2_36/(P1_36+P2_36);
-    assign w26 = P1_36/(P1_36+P2_36);
-
-    assign X0f = (w11*X1_0 + w21*X2_0) >>> 15;
-    assign X1f = (w12*X1_1 + w22*X2_1) >>> 15;
-    assign X2f = (w13*X1_2 + w23*X2_2) >>> 15;
-    assign X3f = (w14*X1_3 + w24*X2_3) >>> 15;
-    assign X4f = (w15*X1_4 + w25*X2_4) >>> 15;
-    assign X5f = (w16*X1_5 + w26*X2_5) >>> 15;
-
-    assign Pf1 = P1_0 * P2_0 *invt1;
-    assign Pf2 = P1_6*P2_6*invt2;
-    assign Pf3 = P1_14*P2_14*invt3;
-    assign Pf4 = P1_21*P2_21*invt4;
-    assign Pf5 = P1_28*P2_28*invt5;
-    assign Pf6 = P1_36*P2_36*invt6;
+    /* Fused covariance values */
+    assign Pf1 = (P1_0  * P2_0  * inv0) >>> 15;
+    assign Pf2 = (P1_6  * P2_6  * inv1) >>> 15;
+    assign Pf3 = (P1_14 * P2_14 * inv2) >>> 15;
+    assign Pf4 = (P1_21 * P2_21 * inv3) >>> 15;
+    assign Pf5 = (P1_28 * P2_28 * inv4) >>> 15;
+    assign Pf6 = (P1_36 * P2_36 * inv5) >>> 15;
 
 endmodule
